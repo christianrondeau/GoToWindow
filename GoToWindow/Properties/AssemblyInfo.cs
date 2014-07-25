50b42b472544b23ede0a947ffacdcaaded04a9f0
Christian Rondeau <christian.rondeau@gmail.com>
2014-07-25 00:16:59 -0400

    Provide some information about the assembly

--- GoToWindow/Properties/AssemblyInfo.cs
+++ GoToWindow/Properties/AssemblyInfo.cs
@@ -2,35 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("GoToWindow")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Go To Window")]
+[assembly: AssemblyDescription("The keyboard-maniac alt-tab alternative")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GoToWindow")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Christian Rondeau")]
+[assembly: AssemblyProduct("Go To Window")]
+[assembly: AssemblyCopyright("Copyright © Christian Rondeau 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d7f91cc1-ef8a-4d2c-a3a5-901f1ff66fd3")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
