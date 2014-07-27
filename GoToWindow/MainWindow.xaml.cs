2630545c4325c0c77e87ee84fbcd90dbac714dc4
Christian Rondeau <christian.rondeau@gmail.com>
2014-07-27 11:59:22 -0400

    ... Can search in the opened apps

--- GoToWindow/MainWindow.xaml.cs
+++ GoToWindow/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -46,6 +47,31 @@ namespace GoToWindow
         {
             if (e.Key == Key.Escape)
                 Close();
+        }
+
+        private void searchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var view = (CollectionView)CollectionViewSource.GetDefaultView(windowsListView.ItemsSource);
+            view.Filter = item => SearchFilter((IWindowEntry)item, searchTextBox.Text);
+        }
+
+        private bool SearchFilter(IWindowEntry window, string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return true;
+
+            return StringContains(window.ProcessName + " " + window.Title, text);
+        }
+
+        private static bool StringContains(string text, string partial)
+        {
+            return partial.Split(' ').All(word => CultureInfo.CurrentUICulture.CompareInfo.IndexOf(text, word, CompareOptions.IgnoreCase) > -1);
+        }
+
+        private void searchBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            searchTextBox.Focus();
+            searchTextBox.CaretIndex = searchTextBox.Text.Length;
         }
     }
 }
