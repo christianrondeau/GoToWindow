1817c3bc4e13d1c5c43fef0cc086a6b50749c71d
Christian Rondeau <christian.rondeau@gmail.com>
2014-07-27 15:20:43 -0400

    Prevent multiple instances from being run; make alt-tab handler async, preventing windows from taking over

--- GoToWindow/App.xaml.cs
+++ GoToWindow/App.xaml.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace GoToWindow
 {
@@ -17,9 +19,18 @@ namespace GoToWindow
     {
         private readonly IGoToWindowContext _context = new GoToWindowContext();
         private InterceptAltTab _keyHandler;
+        private Mutex _mutex;
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            bool mutexCreated;
+            _mutex = new Mutex(true, "GoToWindow", out mutexCreated);
+            if (!mutexCreated)
+            {
+                Application.Current.Shutdown(1);
+                return;
+            }
+
             TaskbarIcon trayIcon = new TaskbarIcon();
             trayIcon.Icon = GoToWindow.Properties.Resources.AppIcon;
             trayIcon.ToolTipText = "Go To Window";
@@ -36,13 +47,25 @@ namespace GoToWindow
             _keyHandler = new InterceptAltTab(HandleAltTab);
         }
 
+        delegate void CommandDelegate(object parameters);
+
         private void HandleAltTab()
         {
-            new OpenMainWindowCommand(_context).Execute(null);
+            var cmd = new OpenMainWindowCommand(_context);
+            Application.Current.Dispatcher.BeginInvoke(
+                new CommandDelegate(cmd.Execute),
+                DispatcherPriority.Normal,
+                new Object[] { null });
         }
 
         private void Application_Exit(object sender, ExitEventArgs e)
         {
+            if (_mutex != null)
+            {
+                _mutex.ReleaseMutex();
+                _mutex.Dispose();
+            }
+
             if (_keyHandler != null)
             {
                 _keyHandler.Dispose();
