61071277594cdf6d79e640e513d9c611c8bbe13b
Christian Rondeau <christian.rondeau@gmail.com>
2014-07-30 19:36:29 -0400

    Use Angular + Marked to make it easier to upgrade

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+﻿(function () {
+	"use strict;"
+
+	var app = angular.module('goToWindowSiteApp', ["hc.marked"]);
+
+	app.config(['markedProvider', function (markedProvider) {
+		markedProvider.setOptions({ gfm: true });
+	}]);
+
+})();
