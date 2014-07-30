5f372bee498e4407a65a52bd137d1d0de24eb13c
Christian Rondeau <christian.rondeau@mediaclip.ca>
2014-07-30 10:23:13 -0400

    Remove GCL_HICON support; it crashed on some machines, and we can get the process icon anyway.

--- GoToWindow.Api/WindowIcon.cs
+++ GoToWindow.Api/WindowIcon.cs
@@ -6,26 +6,12 @@ namespace GoToWindow.Api
     public static class WindowIcon
     {
         // ReSharper disable InconsistentNaming
-        public const int GCL_HICONSM = -34;
-        public const int GCL_HICON = -14;
-
         public const int ICON_SMALL = 0;
         public const int ICON_BIG = 1;
         public const int ICON_SMALL2 = 2;
 
         public const int WM_GETICON = 0x7F;
         // ReSharper restore InconsistentNaming
-
-        public static IntPtr GetClassLongPtr(IntPtr hWnd, int nIndex)
-        {
-            return IntPtr.Size > 4 ? GetClassLongPtr64(hWnd, nIndex) : new IntPtr(GetClassLongPtr32(hWnd, nIndex));
-        }
-
-        [DllImport("user32.dll", EntryPoint = "GetClassLong")]
-        public static extern uint GetClassLongPtr32(IntPtr hWnd, int nIndex);
-
-        [DllImport("user32.dll", EntryPoint = "GetClassLongPtr")]
-        public static extern IntPtr GetClassLongPtr64(IntPtr hWnd, int nIndex);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
@@ -38,10 +24,6 @@ namespace GoToWindow.Api
                 iconHandle = SendMessage(hwnd, WM_GETICON, ICON_SMALL, 0);
             if (iconHandle == IntPtr.Zero)
                 iconHandle = SendMessage(hwnd, WM_GETICON, ICON_SMALL2, 0);
-            if (iconHandle == IntPtr.Zero)
-                iconHandle = GetClassLongPtr(hwnd, GCL_HICON);
-            if (iconHandle == IntPtr.Zero)
-                iconHandle = GetClassLongPtr(hwnd, GCL_HICONSM);
 
             return iconHandle;
         }
