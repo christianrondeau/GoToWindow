e86a1ee7ba4b038cc0b95f2b8a18891fdb72f67f
Christian Rondeau <christian.rondeau@gmail.com>
2014-07-30 23:27:24 -0400

    Add some debugging use cases to the fake app (testing)

--- GoToWindow.FakeApp/MainWindow.xaml.cs
+++ GoToWindow.FakeApp/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Input;
 
 namespace GoToWindow.FakeApp
 {
     public partial class MainWindow : Window
     {
+        private const int MaxKeyInputMessages = 20;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -18,6 +21,46 @@ namespace GoToWindow.FakeApp
                 throw new ApplicationException("Fake App requires a title as the command line argument");
 
             Title = args[1];
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            InsertKeyMessage("Down: " + GetKeyEventString(e));
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            InsertKeyMessage("Up:   " + GetKeyEventString(e));
+        }
+
+        private void InsertKeyMessage(string msg)
+        {
+            KeyInputsListBox.Items.Insert(0, msg);
+            while (KeyInputsListBox.Items.Count > MaxKeyInputMessages)
+                KeyInputsListBox.Items.RemoveAt(KeyInputsListBox.Items.Count - 1);
+        }
+
+        private static string GetKeyEventString(KeyEventArgs e)
+        {
+            if (e.Key == Key.System)
+                return e.SystemKey.ToString() + " (System)";
+
+            return e.Key.ToString();
+        }
+
+        private void Menu_Crash(object sender, RoutedEventArgs e)
+        {
+            throw new ApplicationException("This is an exception manually thrown for debugging purposes");
+        }
+
+        private void Menu_MessageBox(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a dialog");
+        }
+
+        private void Menu_Close(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
