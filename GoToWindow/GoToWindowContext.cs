68060eeb785f20c1f4f3368a92bd162ccf15b7fc
Christian Rondeau <christian.rondeau@gmail.com>
2014-08-22 00:14:54 -0400

    ... Better handling (with ugly code) of asynchronous showing/hiding of main window. To improve and clean up.

--- GoToWindow/GoToWindowContext.cs
+++ GoToWindow/GoToWindowContext.cs
@@ -7,6 +7,8 @@ using GoToWindow.Api;
 using GoToWindow.ViewModels;
 using GoToWindow.Windows;
 using log4net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace GoToWindow
 {
@@ -24,10 +26,14 @@ namespace GoToWindow
 	{
 		private static readonly ILog Log = LogManager.GetLogger(typeof(GoToWindowContext).Assembly, "GoToWindow");
 
-	    private bool _operationInProgress;
+		private Object _lock = new Object();
+	    private bool _showInProgress;
+		private bool _hideInProgress;
+		private bool _hidePending;
 		private MainViewModel _mainViewModel;
         private MainWindow _mainWindow;
         private KeyboardHook _hooks;
+		private IWindowEntry _mainWindowEntry;
 
 		public IGoToWindowPluginsContainer PluginsContainer { get; private set; }
 
@@ -43,50 +49,89 @@ namespace GoToWindow
 
         public void Show()
         {
-            if (_operationInProgress)
-                return;
-
-            if (_mainWindow.Visibility == Visibility.Visible && _mainWindow.IsLoaded)
-            {
-                Log.Debug("Sending Tab Again to Main Window.");
-
-				_mainWindow.TabAgain();
-			}
-			else
+			lock (_lock)
 			{
-				Log.Debug("Showing Main Window.");
-			    _operationInProgress = true;
+				if (_showInProgress)
+					return;
 
-                _mainWindow.Show();
-
-				var interopHelper = new WindowInteropHelper(_mainWindow);
-				var thisEntry = WindowEntryFactory.Create(interopHelper.Handle);
-
-				_mainWindow.SetFocus();
-
-				if (!thisEntry.HasFocus())
+				if (_mainWindow.Visibility == Visibility.Visible && _mainWindow.IsLoaded)
 				{
-					Log.Debug("Window does not have focus when shown. Forcing focus.");
-					thisEntry.Focus();
-				}
+					Log.Debug("Sending Tab Again to Main Window.");
 
-				Application.Current.Dispatcher.InvokeAsync(LoadViewModel, DispatcherPriority.Background);
-            }
+					_mainWindow.TabAgain();
+				}
+				else
+				{
+					Log.Debug("Showing Main Window.");
+					_showInProgress = true;
+
+					_mainWindow.Show();
+
+					if (_mainWindowEntry == null)
+					{
+						var interopHelper = new WindowInteropHelper(_mainWindow);
+						_mainWindowEntry = WindowEntryFactory.Create(interopHelper.Handle);
+
+						Log.DebugFormat("GoToWindow main window created with hWnd {0}", _mainWindowEntry.HWnd);
+					}
+
+					_mainWindow.SetFocus();
+					_mainWindowEntry.Focus();
+
+					Application.Current.Dispatcher.InvokeAsync(LoadViewModel, DispatcherPriority.Background);
+				}
+			}
         }
+
+		private void ForceWindowFocus()
+		{
+			if (!_mainWindowEntry.IsForeground())
+			{
+				_mainWindow.SetFocus();
+				Log.Debug("Window does not have focus when shown. Forcing focus.");
+				_mainWindowEntry.Focus();
+			}
+		}
 
         public void Hide()
         {
-            if (_operationInProgress || _mainWindow == null || !_mainWindow.IsLoaded) 
-                return;
+			lock (_lock)
+			{
+				if (_hideInProgress || _hidePending)
+					return;
 
-			_operationInProgress = true;
+				if (_showInProgress || _mainWindow == null || !_mainWindow.IsLoaded)
+				{
+					if (!_hidePending)
+					{
+						Log.Debug("Cannot Hide because Show is still in progress. Pending Hide.");
+						_hidePending = true;
+						return;
+					}
+				}
 
-            Log.Debug("Hiding Main Window.");
-            _mainWindow.BeginInit();
-            _mainViewModel.Empty();
-            _mainWindow.EndInit();
-			//TODO: Force updating of the list view, otherwise it flickers when re-opening
-            Application.Current.Dispatcher.Invoke(HideWindow, DispatcherPriority.ApplicationIdle);
+				_hideInProgress = true;
+
+				Log.Debug("Hiding Main Window.");
+				try
+				{
+					_mainWindow.BeginInit();
+					_mainViewModel.Empty();
+					_mainWindow.EndInit();
+				}
+				catch (InvalidOperationException exc)
+				{
+					Log.Warn("Failed showing window", exc);
+				}
+
+				Application.Current.Dispatcher.InvokeAsync(HideWindowAsync, DispatcherPriority.ApplicationIdle);
+			}
+		}
+
+		private async void HideWindowAsync()
+		{
+			await Task.Delay(100); // Ugly hack to let the windows list clear, avoiding a flicker when re-opening
+			HideWindow();
 		}
 
 		public void EnableAltTabHook(bool enabled, int shortcutPressesBeforeOpen)
@@ -126,17 +171,77 @@ namespace GoToWindow
 
 		private void LoadViewModel()
 		{
-			_mainWindow.BeginInit();
-			_mainViewModel.Load(PluginsContainer.Plugins);
-			_mainWindow.ApplyFilter();
-			_mainWindow.EndInit();
-			_operationInProgress = false;
+			lock (_lock)
+			{
+				_mainWindow.BeginInit();
+				_mainViewModel.Load(PluginsContainer.Plugins);
+				_mainWindow.ApplyFilter();
+				_mainWindow.EndInit();
+				Log.Debug("View Model loaded.");
+				Application.Current.Dispatcher.InvokeAsync(EnsureWindowIsForeground, DispatcherPriority.Background);
+				_showInProgress = false;
+			}
+
+			RunPendingHide();
+		}
+
+		private async void EnsureWindowIsForeground()
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				await Task.Delay(10);
+
+				if (_hideInProgress || _hidePending)
+				{
+					Log.Debug("Ensuring foreground: Hide is already in progress. Stop watching.");
+					return;
+				}
+
+				if (HideWindowIfNotForeground())
+					return;
+			}
+		}
+
+		private bool HideWindowIfNotForeground()
+		{
+			if (_mainWindowEntry.IsForeground())
+				return false;
+
+			#if(DEBUG)
+			var foregroundWindow = WindowEntryFactory.Create(WindowToForeground.GetForegroundWindow());
+			Log.DebugFormat("Window does not have focus when initialization is complete. Current foreground window is {0} (Process '{1}')", foregroundWindow.HWnd, foregroundWindow.ProcessName);
+			#endif
+
+			Hide();
+			return true;
+		}
+
+		private void RunPendingHide()
+		{
+			bool shouldRun = false;
+
+			lock(_lock)
+			{
+				if (!_hidePending)
+					return;
+
+				Log.Debug("Executing pending Hide.");
+				_hidePending = false;
+				shouldRun = true;
+			}
+
+			if (shouldRun)
+				Hide();
 		}
 
 		private void HideWindow()
 		{
-			_mainWindow.Hide();
-			_operationInProgress = false;
+			lock (_lock)
+			{
+				_mainWindow.Hide();
+				Log.Debug("Main window hidden.");
+				_hideInProgress = false;
+			}
 		}
 
 		private void HandleAltTab()
