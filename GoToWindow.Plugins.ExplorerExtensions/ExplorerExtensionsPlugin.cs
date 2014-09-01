e9432f63a7dfad33653617b378818750ea74820f
Christian Rondeau <christian@mediaclip.ca>
2014-09-01 14:45:06 -0400

    ... ExplorerExtensions plugin (empty)

---
+++ GoToWindow.Plugins.ExplorerExtensions/ExplorerExtensionsPlugin.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using GoToWindow.Extensibility;
+
+namespace GoToWindow.Plugins.ExplorerExtensions
+{
+	[Export(GoToWindowConstants.PluginContractName, typeof(IGoToWindowPlugin))]
+	public class ExplorerExtensionsPlugin : IGoToWindowPlugin
+	{
+		public string Id { get { return "GoToWindow.ExplorerExtensions"; } }
+
+		public string Title { get { return "GoToWindow Expand Browser Tabs"; } }
+
+		public GoToWindowPluginSequence Sequence { get { return GoToWindowPluginSequence.AfterCore; } }
+
+		public void BuildList(List<ISearchResult> list)
+		{
+			/*
+			     IntPtr MyHwnd = FindWindow(null, "Directory");
+    var t = Type.GetTypeFromProgID("Shell.Application");
+    dynamic o = Activator.CreateInstance(t);
+    try
+    {
+        var ws = o.Windows();
+        for (int i = 0; i < ws.Count; i++)
+        {
+            var ie = ws.Item(i);
+            if (ie == null || ie.hwnd != (long)MyHwnd) continue;
+            var path = System.IO.Path.GetFileName((string)ie.FullName);
+            if (path.ToLower() == "explorer.exe")
+            {
+                var explorepath = ie.document.focuseditem.path;
+            }
+        }
+    }
+    finally
+    {
+        Marshal.FinalReleaseComObject(o);
+    } 
+			 */
+		}
+	}
+}
---
+++ GoToWindow.Plugins.ExplorerExtensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Go To Window Explorer Extensions Plugin")]
+[assembly: AssemblyDescription("The keyboard-maniac alt-tab alternative")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Christian Rondeau")]
+[assembly: AssemblyProduct("Go To Window")]
+[assembly: AssemblyCopyright("Copyright © Christian Rondeau 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5263b6d-84cc-47c1-80c5-280616878fab")]
+
+[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyFileVersion("0.1.2")]
+[assembly: AssemblyInformationalVersion("0.1.2")]
