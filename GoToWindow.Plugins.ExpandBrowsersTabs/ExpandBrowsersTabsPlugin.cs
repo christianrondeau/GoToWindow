4b8b717a61b5f777c0b9e126a6d90c1b6a8005a9
Christian Rondeau <christian.rondeau@mediaclip.ca>
2014-09-02 19:08:40 -0400

    Add a timeout to expand browser tabs plugin

--- GoToWindow.Plugins.ExpandBrowsersTabs/ExpandBrowsersTabsPlugin.cs
+++ GoToWindow.Plugins.ExpandBrowsersTabs/ExpandBrowsersTabsPlugin.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using GoToWindow.Extensibility;
 using GoToWindow.Extensibility.Controls;
 using GoToWindow.Extensibility.ViewModel;
@@ -10,12 +12,17 @@ using GoToWindow.Plugins.ExpandBrowsersTabs.Contracts;
 using GoToWindow.Plugins.ExpandBrowsersTabs.Firefox;
 using GoToWindow.Plugins.ExpandBrowsersTabs.InternetExplorer;
 using GoToWindow.Plugins.ExpandBrowsersTabs.ViewModel;
+using log4net;
 
 namespace GoToWindow.Plugins.ExpandBrowsersTabs
 {
 	[Export(GoToWindowConstants.PluginContractName, typeof(IGoToWindowPlugin))]
 	public class ExpandBrowsersTabsPlugin : IGoToWindowPlugin
 	{
+		private const int TimeoutMilliseconds = 1500;
+
+		private static readonly ILog Log = LogManager.GetLogger(typeof(ExpandBrowsersTabsPlugin).Assembly, "GoToWindow");
+
 		public string Id { get { return "GoToWindow.ExpandBrowsersTabs"; } }
 
 		public string Title { get { return "GoToWindow Expand Browser Tabs"; } }
@@ -33,7 +40,7 @@ namespace GoToWindow.Plugins.ExpandBrowsersTabs
 		{
 			var finders = new Dictionary<string, ITabsFinder>();
 
-			for(var index = list.Count - 1; index >= 0; index--)
+			for (var index = list.Count - 1; index >= 0; index--)
 			{
 				var item = list[index] as IWindowSearchResult;
 
@@ -41,16 +48,28 @@ namespace GoToWindow.Plugins.ExpandBrowsersTabs
 					continue;
 
 				ITabsFinder finder;
-				if(!finders.TryGetValue(item.ProcessName, out finder))
+				var browserName = item.ProcessName;
+				if (!finders.TryGetValue(browserName, out finder))
 				{
 					Func<ITabsFinder> finderCtor;
-					if (TabsFinders.TryGetValue(item.ProcessName, out finderCtor))
+					if (TabsFinders.TryGetValue(browserName, out finderCtor))
 						finder = finderCtor();
 					else
 						continue;
 				}
 
-				var tabs = finder.GetTabsOfWindow(item.HWnd);
+				IEnumerable<ITab> tabs = null;
+
+				var tokenSource = new CancellationTokenSource();
+				var token = tokenSource.Token;
+
+				var task = Task.Factory.StartNew(() => tabs = finder.GetTabsOfWindow(item.HWnd).ToArray(), token);
+
+				if (!task.Wait(TimeoutMilliseconds, token))
+				{
+					Log.WarnFormat("Timeout trying to get tabs for '{0}'", browserName);
+					continue;
+				}
 
 				var tabResults = tabs.Select(tab => ConvertTabToResult(item, tab)).ToArray();
 
