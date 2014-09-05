86444eb52f618dd268d35e4b3ba87434635ae118
Christian Rondeau <christian.rondeau@gmail.com>
2014-09-05 18:49:53 -0400

    ... Show the version number in windows entries rather than on every entry

---
+++ GoToWindow.Extensibility/Converters/IndexConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace GoToWindow.Extensibility.Converters
+{
+	public class IndexConverter : IValueConverter
+	{
+		public object Convert(object value, Type TargetType, object parameter, CultureInfo culture)
+		{
+			ListViewItem item = (ListViewItem)value;
+			ListView listView = ItemsControl.ItemsControlFromItemContainer(item) as ListView;
+			int index = listView.ItemContainerGenerator.IndexFromContainer(item);
+			return (index + 1).ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
