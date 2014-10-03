ec5389639c1c079ee49c2e0185e5f92df2008332
Christian Rondeau <christian.rondeau@gmail.com>
2014-10-02 21:10:51 -0400

    #33: Fix GoToWindow crashing on Vista when not in Admin mode

--- GoToWindow.Api/WmiProcessWatcher.cs
+++ GoToWindow.Api/WmiProcessWatcher.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
@@ -10,7 +11,9 @@ namespace GoToWindow.Api
     /// Thanks to http://stackoverflow.com/users/270315/jaroslaw-waliszko for the idea
     /// </remarks>
     public static class WmiProcessWatcher
-    {
+	{
+		private static readonly ILog Log = LogManager.GetLogger(typeof(WmiProcessWatcher).Assembly, "GoToWindow");
+
         private static IDictionary<uint, string> _map;
         private static ManagementEventWatcher _processStartedWatcher;
         private static ManagementEventWatcher _processStoppedwatcher;
@@ -20,15 +23,23 @@ namespace GoToWindow.Api
             if (!WindowsRuntimeHelper.GetHasElevatedPrivileges())
                 return;
 
-			_map = new ConcurrentDictionary<uint, string>();
+			try
+			{
+				_map = new ConcurrentDictionary<uint, string>();
 
-            _processStartedWatcher = new ManagementEventWatcher("SELECT ProcessID, ProcessName FROM Win32_ProcessStartTrace");
-            _processStartedWatcher.EventArrived += ProcessStarted;
-            _processStartedWatcher.Start();
+				_processStartedWatcher = new ManagementEventWatcher("SELECT ProcessID, ProcessName FROM Win32_ProcessStartTrace");
+				_processStartedWatcher.EventArrived += ProcessStarted;
+				_processStartedWatcher.Start();
 
-            _processStoppedwatcher = new ManagementEventWatcher("SELECT ProcessID FROM Win32_ProcessStopTrace");
-            _processStoppedwatcher.EventArrived += ProcessStopped;
-            _processStoppedwatcher.Start();
+				_processStoppedwatcher = new ManagementEventWatcher("SELECT ProcessID FROM Win32_ProcessStopTrace");
+				_processStoppedwatcher.EventArrived += ProcessStopped;
+				_processStoppedwatcher.Start();
+			}
+			catch (ManagementException exc)
+			{
+				Log.Warn("Could not start listening to WMI events", exc);
+				Stop();
+			}
         }
 
         private static void ProcessStarted(object sender, EventArrivedEventArgs e)
