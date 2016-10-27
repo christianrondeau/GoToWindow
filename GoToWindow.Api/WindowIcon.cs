e91886d85e0ec53784d54eda1a75f1dc49f7ac5c
Christian Rondeau <christian.rondeau@gmail.com>
2016-10-27 08:23:40 -0400

    Reformat spacing and fix uncommented url in WindowIcon

--- GoToWindow.Api/WindowIcon.cs
+++ GoToWindow.Api/WindowIcon.cs
@@ -15,22 +15,24 @@ namespace GoToWindow.Api
 		// ReSharper restore InconsistentNaming
 
 		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
-		static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+		private static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
 
-		public static IntPtr GetAppIcon(IntPtr hwnd){
-            var task = Task.Run(() => GetAppIconCore(hwnd));
-            return task.Wait(TimeSpan.FromMilliseconds(100)) ? task.Result : hwnd;
+		public static IntPtr GetAppIcon(IntPtr hwnd)
+		{
+			var task = Task.Run(() => GetAppIconCore(hwnd));
+			return task.Wait(TimeSpan.FromMilliseconds(100)) ? task.Result : hwnd;
 		}
 
-	    private static IntPtr GetAppIconCore(IntPtr hwnd){
-             http://codeutopia.net/blog/2007/12/18/find-an-applications-icon-with-winapi/
-            var iconHandle = SendMessage(hwnd, WM_GETICON, ICON_BIG, 0);
-            if (iconHandle == IntPtr.Zero)
-            	iconHandle = SendMessage(hwnd, WM_GETICON, ICON_SMALL, 0);
-            if (iconHandle == IntPtr.Zero)
-            	iconHandle = SendMessage(hwnd, WM_GETICON, ICON_SMALL2, 0);
+		private static IntPtr GetAppIconCore(IntPtr hwnd)
+		{
+			// http://codeutopia.net/blog/2007/12/18/find-an-applications-icon-with-winapi/
+			var iconHandle = SendMessage(hwnd, WM_GETICON, ICON_BIG, 0);
+			if (iconHandle == IntPtr.Zero)
+				iconHandle = SendMessage(hwnd, WM_GETICON, ICON_SMALL, 0);
+			if (iconHandle == IntPtr.Zero)
+				iconHandle = SendMessage(hwnd, WM_GETICON, ICON_SMALL2, 0);
 
-            return iconHandle;
-        }
-    }
+			return iconHandle;
+		}
+	}
 }
