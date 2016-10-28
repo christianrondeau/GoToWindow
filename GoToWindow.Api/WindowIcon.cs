41bee2b4d533e3e86b716e9a6b0235be03d9b1f1
Christian Rondeau <christian.rondeau@gmail.com>
2016-10-27 22:32:32 -0400

    #46: Use SendMessageTimeout to avoid creating new threads for each icon

--- GoToWindow.Api/WindowIcon.cs
+++ GoToWindow.Api/WindowIcon.cs
@@ -1,38 +1,38 @@
 ﻿using System;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 
 namespace GoToWindow.Api
 {
 	public static class WindowIcon
 	{
+		public const int PerIconTimeoutMilliseconds = 50;
+
 		// ReSharper disable InconsistentNaming
 		public const int ICON_SMALL = 0;
 		public const int ICON_BIG = 1;
 		public const int ICON_SMALL2 = 2;
 
 		public const int WM_GETICON = 0x7F;
+
+		public const int SMTO_ABORTIFHUNG = 0x0002;
 		// ReSharper restore InconsistentNaming
 
 		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
-		private static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+		private static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint  msg, uint  wParam, int lParam, uint  fuFlags, uint  uTimeout, out IntPtr lpdwResult);
 
 		public static IntPtr GetAppIcon(IntPtr hwnd)
 		{
-			var task = Task.Run(() => GetAppIconCore(hwnd));
-			return task.Wait(TimeSpan.FromMilliseconds(100)) ? task.Result : hwnd;
-		}
-
-		private static IntPtr GetAppIconCore(IntPtr hwnd)
-		{
 			// http://codeutopia.net/blog/2007/12/18/find-an-applications-icon-with-winapi/
-			var iconHandle = SendMessage(hwnd, WM_GETICON, ICON_BIG, 0);
-			if (iconHandle == IntPtr.Zero)
-				iconHandle = SendMessage(hwnd, WM_GETICON, ICON_SMALL, 0);
-			if (iconHandle == IntPtr.Zero)
-				iconHandle = SendMessage(hwnd, WM_GETICON, ICON_SMALL2, 0);
+			IntPtr iconHandle;
+			var result = SendMessageTimeout(hwnd, WM_GETICON, ICON_BIG, 0, SMTO_ABORTIFHUNG, PerIconTimeoutMilliseconds, out iconHandle);
+			if (result != IntPtr.Zero && iconHandle == IntPtr.Zero)
+				result = SendMessageTimeout(hwnd, WM_GETICON, ICON_SMALL, 0, SMTO_ABORTIFHUNG, PerIconTimeoutMilliseconds, out iconHandle);
+			if (result != IntPtr.Zero && iconHandle == IntPtr.Zero)
+				result = SendMessageTimeout(hwnd, WM_GETICON, ICON_SMALL2, 0, SMTO_ABORTIFHUNG, PerIconTimeoutMilliseconds, out iconHandle);
+			if(result != IntPtr.Zero)
+				return iconHandle;
 
-			return iconHandle;
+			return IntPtr.Zero;
 		}
 	}
 }
