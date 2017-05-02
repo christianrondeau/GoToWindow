07d89c48a7bb0671af968e10efb38d3fd1eb4b6a
Christian Rondeau <christian.rondeau@gmail.com>
2017-05-01 21:16:25 -0400

    Add chocolatey packaging

---
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'GoToWindow'
+$url        = 'https://github.com/christianrondeau/GoToWindow/releases/download/v0.6.1/GoToWindow.Setup.0.6.1.exe'
+$installerType = 'exe'
+$silentArgs = '--silent'
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url
---
+++ chocolatey/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$localApplicationData = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::LocalApplicationData))
+$gotowindowFolder = Join-Path $localApplicationData "GoToWindow"
+$updateExe = Join-Path $gotowindowFolder "Update.exe"
+
+Start-Process -FilePath $updateExe -ArgumentList "--uninstall" -Verb "RunAs" -Wait
