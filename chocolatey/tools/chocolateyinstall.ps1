924287809a6e5e9d114afefdcad68d80ab1de8c1
Christian Rondeau <christian.rondeau@gmail.com>
2017-06-11 20:00:18 -0400

    Update the downloadable setup hash

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   installerType  = 'exe'
   url            = 'https://github.com/christianrondeau/GoToWindow/releases/download/v0.7.2/GoToWindow.Setup.0.7.2.exe'
   checksumType   = 'sha256'
-  checksum       = '1AC20D3225DA6135FFAA943E7A7F0D8F95923DDF3E7B019EE0E09CD77CE49A78'
+  checksum       = '2073F863199E32C7A5DAD06D2004280A0E316A7686ED8496F3FC29854926CEC8'
   silentArgs     = '--silent'
   validExitCodes = @(0)
 }
